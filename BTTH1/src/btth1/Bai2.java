8c6a4e6d6583084649fdaf10695023e06b683ae3
James Au <21522019@gm.uit.edu.vn>
2024-03-07 13:04:54 +0700

    fix: BTTH1 source code

---
+++ BTTH1/src/btth1/Bai2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btth1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+
+public class Bai2 {
+    public static float Sum(float x, int n) {
+        float T1 = 1;
+        float T2 = 0;
+        float S = 0;
+
+        for (int i = 1; i <= n; i++) {
+            T1 = T1 * x;
+            T2 = T2 + i;
+            S = S + (float) T1 / T2;
+        }
+    
+        return S;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float x;
+        System.out.println("\nNhập x: ");
+        x = sc.nextFloat();
+        
+        System.err.println("\nNhập n: ");
+        int n;
+        n = sc.nextInt();
+        
+        float S;
+        S = Sum(x, n);
+        System.out.println("\nGiá trị của biểu thức là S = " + S);
+    }
+}
