f1a381804df919d38c775655519ec832f68b1ca4
James Au <21522019@gm.uit.edu.vn>
2024-03-25 07:44:19 +0700

    feat: add Bai4 file

---
+++ BTTH1/src/btth1/Bai4.java
@@ -0,0 +1,79 @@
+package btth1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Bai4 {
+    private static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    private static boolean isPalindromeNumber(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+
+    private static boolean isPerfectSquareNumber(int number) {
+        int sqrt = (int) Math.sqrt(number);
+        return sqrt * sqrt == number;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+
+        do {
+            try {
+                System.out.print("Nhap vao so duong n: ");
+                n = input.nextInt();
+
+                if (n <= 0) {
+                    System.out.println("Vui long nhap mot so nguyen duong!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("So n phai so nguyen!");
+                input.next(); 
+                n = 0; 
+            }
+        } while (n <= 0);
+
+        System.out.print("Cac uoc so cua " + n + " la: ");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        int digitCount = countDigits(n);
+        System.out.println("So " + n + " co " + digitCount + " chu so.");
+
+        boolean isPalindrome = isPalindromeNumber(n);
+        if (isPalindrome) {
+            System.out.println("So " + n + " la so doi xung.");
+        } else {
+            System.out.println("So " + n + " khong la so doi xung.");
+        }
+
+        boolean isPerfectSquare = isPerfectSquareNumber(n);
+        if (isPerfectSquare) {
+            System.out.println("So " + n + " la so chinh phuong.");
+        } else {
+            System.out.println("So " + n + " khong la so chinh phuong.");
+        }
+
+        input.close();
+    }
+}
