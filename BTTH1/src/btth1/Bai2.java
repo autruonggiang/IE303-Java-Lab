65b5a6fd7762934045eadc52193c8965664922d2
James Au <21522019@gm.uit.edu.vn>
2024-03-26 22:19:18 +0700

    fix: add comments in Bai2 file

--- BTTH1/src/btth1/Bai2.java
+++ BTTH1/src/btth1/Bai2.java
@@ -3,14 +3,15 @@ package btth1;
 import java.util.Scanner;
 
 public class Bai2 {
+    // Hàm tính tổng
     public static float Sum(float x, int n) {
         float T1 = 1;
         float T2 = 0;
         float S = 0;
 
         for (int i = 1; i <= n; i++) {
-            T1 = T1 * x;
-            T2 = T2 + i;
+            T1 = T1 * x; // Tính x^n
+            T2 = T2 + i; // Tính 1 + 2 + 3 + ... + n
             S = S + (float) T1 / T2;
         }
     
