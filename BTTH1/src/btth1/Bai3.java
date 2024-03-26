9269c792a37a686252a66435bd2a3660c6f14783
James Au <21522019@gm.uit.edu.vn>
2024-03-26 22:30:15 +0700

    fix: add comments and fix output noti in Bai3 file

--- BTTH1/src/btth1/Bai3.java
+++ BTTH1/src/btth1/Bai3.java
@@ -3,14 +3,17 @@ package btth1;
 import java.util.Scanner;
 
 public class Bai3 {
+    // Kiểm tra ba giá trị có phải là ba cạnh tam giác không
     public static boolean isValidTriangle(double a, double b, double c) {
         return a + b > c && b + c > a && c + a > b;
     }
-
+    
+    // Tính chu vi tam giác
     public static double calculatePerimeter(double a, double b, double c) {
         return a + b + c;
     }
 
+    // Tính diện tích tam giác
     public static double calculateArea(double a, double b, double c) {
         double p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
@@ -20,21 +23,21 @@ public class Bai3 {
         Scanner input = new Scanner(System.in);
         float a, b, c;
 
-        System.out.print("\nNhap vao canh thu nhat: ");
+        System.out.print("\nNhập vào cạnh thứ nhất: ");
         a = input.nextFloat();
-	    System.out.print("\nNhap vao canh thu hai: ");
+	    System.out.print("\nNhập vào cạnh thứ hai: ");
         b = input.nextFloat();
-	    System.out.print("\nNhap vao canh thu ba: ");
+	    System.out.print("\nNhập vào cạnh thứ ba: ");
         c = input.nextFloat();
 
         if (isValidTriangle(a, b, c)) {
             double perimeter = calculatePerimeter(a, b, c);
-            System.out.println("\nChu vi hinh tam giac la: " + perimeter);
+            System.out.println("\nChu vi hình tam giác là: " + perimeter);
 
             double area = calculateArea(a, b, c);
-            System.out.println("\nDien tich hinh tam giac la: " + area);
+            System.out.println("\nDiện tích hình tam giác là: " + area);
         } else {
-            System.out.println("Ba canh tren khong the tao thanh mot tam giac hop le!");
+            System.out.println("\nBa cạnh trên không thể tạo thành một tam giác hợp lệ!");
         }
 
         input.close();
